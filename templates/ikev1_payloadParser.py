e6b8062c16ee1d883068d3243ca49b210a3af587
Christian Krug <mrminifreddy@gmail.com>
2021-01-10 15:22:28 +0100

    Remove unused functions

--- templates/ikev1_payloadParser.py
+++ templates/ikev1_payloadParser.py
@@ -12,6 +12,7 @@ def getIniatorSAPacket(packets):
         if pkt.next_payload == 1:  # 1 == SA
             return pkt.getlayer(scapy.layers.isakmp.ISAKMP)
 
+
 def getResponderSAPacket(packets):
     # Find first packet that contains SA
     # Equivalent to return packets[1].getlayer(scapy.layers.isakmp.ISAKMP) if packets are in order
@@ -23,11 +24,13 @@ def getResponderSAPacket(packets):
             else:
                 i += 1
 
+
 def getInitiatorKENoncePacket(packets):
     # Find first packet that contains KE
     for pkt in packets:
         if pkt.next_payload == 4:  # 4 == KE
             return pkt.getlayer(scapy.layers.isakmp.ISAKMP)
+
 
 def getResponderKENoncePacket(packets):
     # Find first packet that contains KE
@@ -39,25 +42,18 @@ def getResponderKENoncePacket(packets):
             else:
                 i += 1
 
+
 def getInitiatorIdentificationPacket(packets):
     # Find first packet that contains KE
     for pkt in packets:
         if pkt.next_payload == 5:  # 5 == IP
             return pkt.getlayer(scapy.layers.isakmp.ISAKMP)
 
-def getResponderIdentificationPacket(packets):
-    # Find first packet that contains KE
-    i = 1
-    for pkt in packets:
-        if pkt.next_payload == 5:  # 5 == IP
-            if i >= 2:
-                return pkt.getlayer(scapy.layers.isakmp.ISAKMP)
-            else:
-                i += 1
 
 # name == payload name
 def getPayloadFromISAKMP(packet, name):
     return packet[name].load
+
 
 # forResponder == True/False
 def getCookieFromISAKMP(packet, forResponder):
@@ -68,29 +64,7 @@ def getCookieFromISAKMP(packet, forResponder):
         # false -> initiator cookie
         return packet.init_cookie
 
-def getSAPayloadFromInitPacket(packet):
-    a = packet.getlayer("ISAKMP SA")
-    b = binascii.hexlify((bytes(a)))
-    overheadlength = 8  # Always 8 bytes for Next Payload(2), Reserved(2), Payload Length(4)
-    payloadlength = a.length
-    c = b[overheadlength:]
-    limit = payloadlength * 2 - overheadlength
-    d = c[:limit]
-    return binascii.unhexlify(d)
-
-def getResponderIDFromRespPacket(packet):
-    # Responder ID consist of  IDType||ProtoID||Port||load
-    packet.show()
-    a = packet["ISAKMP Identification"]
-    b = binascii.hexlify((bytes(a)))
-    overheadlength = 8  # Always 8 bytes for Next Payload(2), Reserved(2), Payload Length(4)
-    payloadlength = a.length
-    c = b[overheadlength:]
-    limit = payloadlength * 2 - overheadlength
-    d = c[:limit]
-    return binascii.unhexlify(d)
 
 def getEncryptedData(packet):
     # Responder ID consist of  IDType||ProtoID||Port||load
     return packet.load
-
